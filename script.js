da06bbd08e66415ac53fb25a2cd23282d0f57bf3
MD.Refayet Ullah SIddique <61475826+refayet-dev@users.noreply.github.com>
2024-03-25 19:22:44 +0600

    script redirection change

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
-require('dotenv').config();
-const axios = require('axios');
-const express = require('express');
-const path = require('path');
+require("dotenv").config();
+const axios = require("axios");
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.use(express.static('static'));
+app.use(express.static("static"));
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello, world!')
-  // res.sendFile(path.join(__dirname, '/static/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/static/index.html"));
 });
 
+app.get("/auth", (req, res) => {
+  res.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&scope=repo`
+  );
+});
 
-app.get('/auth', (req, res) => {
-    res.redirect(
-      `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&scope=repo`,
-    );
-  });
-  
-
-app.get('/oauth-callback', ({ query: { code } }, res) => {
+app.get("/oauth-callback", ({ query: { code } }, res) => {
   const body = {
     client_id: process.env.GITHUB_CLIENT_ID,
     client_secret: process.env.GITHUB_SECRET,
     code,
   };
-  const opts = { headers: { accept: 'application/json' } };
+  const opts = { headers: { accept: "application/json" } };
   axios
-    .post('https://github.com/login/oauth/access_token', body, opts)
+    .post("https://github.com/login/oauth/access_token", body, opts)
     .then((_res) => _res.data.access_token)
     .then((token) => {
       res.redirect(`/?token=${token}`);
@@ -36,9 +33,7 @@ app.get('/oauth-callback', ({ query: { code } }, res) => {
     .catch((err) => res.status(500).json({ err: err.message }));
 });
 
-
-
-const port= 5000 || process.env.PORT;
+const port = 5000 || process.env.PORT;
 app.listen(port);
 // eslint-disable-next-line no-console
 // console.log('App listening on port 3000');
